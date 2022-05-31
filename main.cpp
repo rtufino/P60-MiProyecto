e9d06a741f13d7c7ef06ae7239ab5114ffcbfe7f
Rodrigo Tufiño <rtufino@ups.edu.ec>
2022-05-31 05:34:25 -0500

    Creada la clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+#include "persona.h"
+
 using namespace std;
 
 int main()
 {
     cout << "Mi primer proyecto en Git y GitHub!" << endl;
+    Persona p;
+    p.nombre = "Rodrigo";
+    p.edad = 39;
+    cout << p.nombre << endl;
+    cout << p.edad << endl;
     return 0;
 }
---
+++ persona.h
@@ -0,0 +1,17 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <string>
+
+using namespace std;
+
+class Persona
+{
+public:
+    string nombre;
+    int edad;
+    Persona();
+
+};
+
+#endif // PERSONA_H
